1ba75bb89e56417618d6e6530e7370eed93894b6
Dhruv Trivedi <95343534+drvcodenta@users.noreply.github.com>
2024-09-17 00:52:40 +0530

    remove unnecessary dependencies

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -10,7 +10,7 @@ function Carousels() {
       image: "https://i0.wp.com/s3.ap-south-1.amazonaws.com/img.paisawapas/images/2021/05/18131128/COD-Mobile.png?fit=1185%2C667&ssl=1",
     },
     {
-        image:"https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
+      image:"https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg"
     }
   ];
 
