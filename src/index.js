7519908e4ca15f978dca0226a141aee1a6f02b2a
abhay-01 <pratapsinghabhay999@gmail.com>
2024-09-26 17:59:57 +0530

    notifications

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './input.css'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./input.css";
+import NotificationProvider from "./components/NotificationProvider";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
+  <NotificationProvider>
     <App />
-  </React.StrictMode>
+  </NotificationProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
