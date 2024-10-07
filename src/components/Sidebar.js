d335f008dec9c0eb66703c88594e76d0131a0012
Shrayash Shukla <ishrayash@gmail.com>
2024-10-07 15:10:47 +0530

    did nothing pretier issue

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -67,28 +67,28 @@ export const Sidebar = ({ children }) => {
 
   return (
     <div className="fixed flex">
-    <div className="min-h-screen w-80 text-white bg-custom-gradient h-screen">
+      <div className="min-h-screen w-80 text-white bg-custom-gradient h-screen">
         <div className="flex justify-center items-center py-4">
           <img src={logo} alt="Game Mate" className="h-12" />
         </div>
 
         <div class="flex items-center space-x-2 bg-black p-4 rounded-lg border border-t-0 border-r-0 border-l-0 ml-6 mr-6 mb-4">
-        <div class="bg-custom-gray p-2 rounded mr-8">
-        <FaSearch className="" />
-        </div>
-        <div className="text-lg font-medium">Explore</div>
+          <div class="bg-custom-gray p-2 rounded mr-8">
+            <FaSearch className="" />
+          </div>
+          <div className="text-lg font-medium">Explore</div>
         </div>
 
-        <div className="px-6 py-2 text-gray-400 text-xs">
-          Navigate
-        </div>
+        <div className="px-6 py-2 text-gray-400 text-xs">Navigate</div>
 
         <div className="navigate px-4">
           {menuItem.map((item, index) => (
             <NavLink
               to={item.path}
               key={index}
-              className={`flex items-center ${location.pathname === item.path? 'text-white':'text-gray-400'} hover:text-white py-2 px-2 mb-1`}
+              className={`flex items-center ${
+                location.pathname === item.path ? "text-white" : "text-gray-400"
+              } hover:text-white py-2 px-2 mb-1`}
             >
               <div className="mr-3 text-xl">{item.icon}</div>
               <div className="text-md">{item.name}</div>
@@ -96,7 +96,10 @@ export const Sidebar = ({ children }) => {
           ))}
         </div>
 
-        <div className="absolute bottom-4 left-4 w-[280px] flex items-center py-2 border-t border-gray-700" onClick={handleLoginClick}>
+        <div
+          className="absolute bottom-4 left-4 w-[280px] flex items-center py-2 border-t border-gray-700"
+          onClick={handleLoginClick}
+        >
           <img
             src={userProfileImage}
             alt="User Profile"
