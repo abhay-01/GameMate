7e96fd07f2461fbc63a4144ce291725aea32f67a
Dhruv Trivedi <95343534+drvcodenta@users.noreply.github.com>
2024-10-09 16:51:06 +0530

    Add functional bottombar and topBar

--- src/App.js
+++ src/App.js
@@ -12,13 +12,17 @@ function App() {
           <Sidebar />
           <div className="flex-1 flex flex-col ml-0 lg:ml-[250px]">
             <Navbar />
+            <div className="" >
             <TopBar />
-            <BottomBar />
-            <div className="lg:mt-[70px]">
+            </div>
+            <div className="flex-1 max-lg:mb-[60px]  max-lg:mt-[40px] lg:mt-[70px]">
+            {/* <div className="flex-1 sm:mt-[80px] lg:mt-[70px]"> */}
+
               {" "}
               {/* Adjusts to compensate for the fixed navbar */}
               <AppRoutes />
             </div>
+            <BottomBar />
           </div>
         </div>
       </BrowserRouter>
